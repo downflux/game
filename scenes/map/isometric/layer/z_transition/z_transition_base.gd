6062a5a8636d5d8e6e5222f27db55015dd438c05
Minke Zhang <minke.zhang@gmail.com>
2023-06-25 04:55:12 -0700

    Update layer detection for all ramps

--- scenes/map/isometric/layer/z_transition/z_transition_base.gd
+++ scenes/map/isometric/layer/z_transition/z_transition_base.gd
@@ -3,7 +3,6 @@ extends Area2D
 class_name ZTransitionBase
 
 @export var up_orientation: ZTransitionBase.Orientation
-@export var up_line: Line2D = null
 
 enum Orientation {
     ERROR_UNSPECIFIED,
@@ -28,18 +27,25 @@ const _UP_VECTOR_LOOKUP: Dictionary = {
     Orientation.NORTHWEST: Vector2( 1, -1),
 }
 
+class Plane2D:
+    var p: Vector2
+    var n: Vector2
+    func _init(_p: Vector2, _n: Vector2):
+        p = _p
+        n = _n
+
 # Planes here define the +z direction; the planes are defined in point-normal
 # form, with the are in point-normal form, with the normal pointing into the +z
 # direction.
 var _UP_VECTOR_PLANE: Dictionary = {
-    Orientation.NORTH: {
-        "P": Vector2(-32, 0),
-        "N": Vector2(-1,  2).normalized(),
-    },
-    Orientation.SOUTH: {
-        "P": Vector2(0, -16),
-        "N": Vector2(1, -2).normalized(),
-    }
+    Orientation.NORTH:     Plane2D.new(Vector2(-32,   0), Vector2(-1,  2).normalized()),
+    Orientation.NORTHEAST: Plane2D.new(Vector2(-32,   0), Vector2(-1,  0)),
+    Orientation.EAST:      Plane2D.new(Vector2(  0, -16), Vector2(-1, -2).normalized()),
+    Orientation.SOUTHEAST: Plane2D.new(Vector2(  0,  16), Vector2( 0, -1)),
+    Orientation.SOUTH:     Plane2D.new(Vector2(  0, -16), Vector2( 1, -2).normalized()),
+    Orientation.SOUTHWEST: Plane2D.new(Vector2( 32,   0), Vector2( 1,  0)),
+    Orientation.WEST:      Plane2D.new(Vector2( 32,   0), Vector2( 1,  2).normalized()),
+    Orientation.NORTHWEST: Plane2D.new(Vector2(-32,  16), Vector2( 0,  1)),
 }
 
 ## _up defines the Cartesian direction which points towards the positive z-axis.
@@ -48,10 +54,11 @@ var _up: Vector2
 func _ready():
     _up = _UP_VECTOR_LOOKUP[up_orientation].normalized()
 
+# TODO(minkezhang): Make sure this works for cells other than (0, 0).
 func _body_is_above(body: DFUnit) -> bool:
-    var p = _UP_VECTOR_PLANE[up_orientation]["P"] - global_position
+    var p = _UP_VECTOR_PLANE[up_orientation].p - global_position
     var dp = body.global_position - p
-    return dp.dot(_UP_VECTOR_PLANE[up_orientation]["N"]) > 0
+    return dp.dot(_UP_VECTOR_PLANE[up_orientation].n) > 0
 
 # TODO(minkezhang): Emit signal to IsometricMap instead.
 # TODO(minkezhang): Move children between MapLayers instead of worrying about z_index.
