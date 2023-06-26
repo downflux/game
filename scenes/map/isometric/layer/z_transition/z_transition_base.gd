66f62099fe1b7de97a040f68b7e3a1a30842d351
Minke Zhang <minke.zhang@gmail.com>
2023-06-26 01:00:54 -0700

    Refactor z_transition_base.gd

--- scenes/map/isometric/layer/z_transition/z_transition_base.gd
+++ scenes/map/isometric/layer/z_transition/z_transition_base.gd
@@ -18,6 +18,7 @@ enum Orientation {
     NORTHWEST,
 }
 
+# TODO(minkezhang): Detect the lower z-side instead.
 const _UP_VECTOR_LOOKUP: Dictionary = {
     Orientation.NORTH:     Vector2( 0, -1),
     Orientation.NORTHEAST: Vector2(-1, -1),
@@ -29,34 +30,7 @@ const _UP_VECTOR_LOOKUP: Dictionary = {
     Orientation.NORTHWEST: Vector2( 1, -1),
 }
 
-class Plane2D:
-    var p: Vector2
-    var n: Vector2
-    func _init(_p: Vector2, _n: Vector2):
-        p = _p
-        n = _n
-
-# Planes here define the +z direction; the planes are defined in point-normal
-# form, with the are in point-normal form, with the normal pointing into the +z
-# direction.
-#
-# TODO(minkezhang): Detect the lower z-side instead.
-var _UP_VECTOR_PLANE: Dictionary = {
-    Orientation.NORTH:     Plane2D.new(Vector2(-32,   0), Vector2(-1,  2).normalized()),
-    Orientation.NORTHEAST: Plane2D.new(Vector2(-32,   0), Vector2(-1,  0)),
-    Orientation.EAST:      Plane2D.new(Vector2(  0, -16), Vector2(-1, -2).normalized()),
-    Orientation.SOUTHEAST: Plane2D.new(Vector2(  0,  16), Vector2( 0, -1)),
-    Orientation.SOUTH:     Plane2D.new(Vector2(  0, -16), Vector2( 1, -2).normalized()),
-    Orientation.SOUTHWEST: Plane2D.new(Vector2( 32,   0), Vector2( 1,  0)),
-    Orientation.WEST:      Plane2D.new(Vector2( 32,   0), Vector2( 1,  2).normalized()),
-    Orientation.NORTHWEST: Plane2D.new(Vector2(-32,  16), Vector2( 0,  1)),
-}
-
-## _up defines the Cartesian direction which points towards the positive z-axis.
-var _up: Vector2
-
 func _ready():
-    _up = _UP_VECTOR_LOOKUP[up_orientation].normalized()
     var parent = get_owner() as MapLayer
     if parent != null:
         _z_layer = parent.z_layer
