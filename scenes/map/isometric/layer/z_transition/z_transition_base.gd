85133ec3506d8030deb339fa8147f69740a660b6
Minke Zhang <minke.zhang@gmail.com>
2023-06-26 23:53:21 -0700

    Fix ramp transition detection logic

--- scenes/map/isometric/layer/z_transition/z_transition_base.gd
+++ scenes/map/isometric/layer/z_transition/z_transition_base.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 class_name ZTransitionBase
 
-@export var up_orientation: ZTransitionBase.Orientation
+@export var z_layer_down_face: ZTransitionBase.Orientation
 
 var _z_layer: int = -1
 
@@ -18,42 +18,52 @@ enum Orientation {
     NORTHWEST,
 }
 
-# TODO(minkezhang): Detect the lower z-side instead.
-const _UP_VECTOR_LOOKUP: Dictionary = {
-    Orientation.NORTH:     Vector2( 0, -1),
-    Orientation.NORTHEAST: Vector2(-1, -1),
-    Orientation.EAST:      Vector2(-1,  0),
-    Orientation.SOUTHEAST: Vector2(-1,  1),
-    Orientation.SOUTH:     Vector2( 0,  1),
-    Orientation.SOUTHWEST: Vector2( 1,  1),
-    Orientation.WEST:      Vector2( 1,  0),
-    Orientation.NORTHWEST: Vector2( 1, -1),
+# Together, _Z_LAYER_DOWN_NORMAL and _Z_LAYER_DOWN_POINT defines a half-plane in
+# isometric 2D space, pointing into the lower z plane. This can be used to test
+# for when a DFUnit object exits a ramp and should be moved into a separate
+# layer.
+const _Z_LAYER_DOWN_NORMAL: Dictionary = {
+    Orientation.NORTH:     Vector2( 1, -2),
+    Orientation.NORTHEAST: Vector2( 1,  0),
+    Orientation.EAST:      Vector2( 1,  2),
+    Orientation.SOUTHEAST: Vector2( 0,  1),
+    Orientation.SOUTH:     Vector2(-1,  2),
+    Orientation.SOUTHWEST: Vector2(-1,  0),
+    Orientation.WEST:      Vector2(-1, -2),
+    Orientation.NORTHWEST: Vector2( 0, -1),
 }
 
-func _ready():
-    var parent = get_owner() as MapLayer
-    if parent != null:
-        _z_layer = parent.z_layer
+const _Z_LAYER_DOWN_POINT: Dictionary = {
+    Orientation.NORTH:     Vector2(0,  0),
+    Orientation.NORTHEAST: Vector2(0,  0),
+    Orientation.EAST:      Vector2(0, 32),
+    Orientation.SOUTHEAST: Vector2(0, 16),
+    Orientation.SOUTH:     Vector2(0, 32),
+    Orientation.SOUTHWEST: Vector2(0,  0),
+    Orientation.WEST:      Vector2(0,  0),
+    Orientation.NORTHWEST: Vector2(0, 16),
+}
 
-# TODO(minkezhang): Check for z_layer offset.
-func _body_is_above(body: DFUnit) -> bool:
-    var p = DFVector.flatten(DFVector.inflate(global_position)) + _UP_VECTOR_LOOKUP[up_orientation] * 32
-    var dp = body.df_unit.position2d() - p
-    return dp.dot(_UP_VECTOR_LOOKUP[up_orientation]) > 0
+func _on_tree_entered():
+    # Ramp scenes must be rendered as part of the map.
+    _z_layer = (get_parent().get_parent() as MapLayer).z_layer
 
-# TODO(minkezhang): Emit signal to IsometricMap instead.
-# TODO(minkezhang): Move children between MapLayers instead of worrying about z_index.
+func _init():
+    tree_entered.connect(_on_tree_entered)
+
+func _is_below_tile(body: DFUnit) -> bool:
+    var p = global_position + _Z_LAYER_DOWN_POINT[z_layer_down_face]
+    var dp = body.global_position - p
+    return dp.dot(_Z_LAYER_DOWN_NORMAL[z_layer_down_face]) > 0
+
 func _on_body_entered(body):
     if body is DFUnit:
-        if _body_is_above(body):  # come down to current layer down (if tile is on next tile down)
-            print(_body_is_above(body))
-            (body as DFUnit).proxy_z_layer_down.emit(_z_layer)
-        else:  # entering from bottom or side -- only increase Z-index
-            pass
+        # A unit is detected moving into the collision area from the negative z
+        # plane, and is approaching from the layer immediately below.
+        if _is_below_tile(body): # and body.z_layer == _z_layer - 1:
+            body.proxy_z_layer_up.emit(_z_layer)
 
 func _on_body_exited(body):
     if body is DFUnit:
-        if _body_is_above(body):  # increase body z layer
-            (body as DFUnit).proxy_z_layer_up.emit(_z_layer)
-        else:  # exiting from bottom or side -- decrease Z-index
-            pass
+        if _is_below_tile(body): # and body.z_layer == _z_layer:
+            body.proxy_z_layer_down.emit(_z_layer)
--- scenes/map/isometric/map.gd
+++ scenes/map/isometric/map.gd
@@ -2,20 +2,20 @@ extends Node2D
 
 var _layers: Dictionary = {}
 
-func _ready():
-    for n in get_children():
-        n = n as MapLayer
-        if n != null:
-            assert(
-                _layers.get(n.z_layer, null) == null,
-                "duplicate map z-layers found",
-            )
-            _layers[n.z_layer] = n
+func _init():
+    for n in get_children().filter(func(x): x is MapLayer):
+        assert(
+            _layers.get(n.z_layer, null) == null,
+            "duplicate map z-layers found",
+        )
+        _layers[n.z_layer] = n
+    # child_entered_tree.connect(_on_child_entered_tree)
 
 func _on_child_entered_tree(node):
     if node is DFUnit:
         node.proxy_z_layer_up.connect(_on_z_layer_up.bind(node as DFUnit))
         node.proxy_z_layer_down.connect(_on_z_layer_down.bind(node as DFUnit))
+        _layers[node.z_layer].add_child(node)
 
 func _on_z_layer_up(collision_z_layer: int, node: DFUnit):
     print("Collision detected on %s to move node up" % collision_z_layer)
