26c78f2433a65d0a7e4886ce9f43093a0a2f3626
Minke Zhang <minke.zhang@gmail.com>
2025-03-02 17:24:34 -0800

    Update Navigation input to take in current unit layer

--- godot/scenes/df_level/df_map/df_map_layer/df_map_layer.gd
+++ godot/scenes/df_level/df_map/df_map_layer/df_map_layer.gd
@@ -4,6 +4,7 @@ class_name DFMapLayer
 
 var _src = Vector2i(0, 0)
 var _dst = Vector2i(0, 0)
+var _layer = DFNavigation.L.LAYER_UNKNOWN
 
 
 func debug_get_tile_coordinates(local: Vector2) -> Vector2i:
@@ -15,17 +16,23 @@ func _input(event: InputEvent):
 	if event is InputEventMouseButton and event.pressed:
 		var e = make_input_local(event)
 		_src = debug_get_tile_coordinates(e.position)
+		if $DFTerrain.get_cell_tile_data(_src).get_custom_data("TraversibleSea"):
+			_layer = DFNavigation.L.LAYER_SEA
+		if $DFTerrain.get_cell_tile_data(_src).get_custom_data("TraversibleGround"):
+			_layer = DFNavigation.L.LAYER_GROUND
 	if event is InputEventMouseButton and event.is_released():
 		var e = make_input_local(event)
 		_dst = debug_get_tile_coordinates(e.position)
 		$DFNavigationUI.show_path(
 			$DFNavigation.get_id_path(
-				DFNavigation.L.LAYER_GROUND,
+				_layer,
 				_src,
 				_dst,
 				true,
 			),
 		)
+		_layer = DFNavigation.L.LAYER_UNKNOWN
+
 
 
 func _ready():
