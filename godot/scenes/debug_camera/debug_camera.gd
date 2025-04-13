11b4406ad291a06c63a906cbd44936a42ad71273
Minke Zhang <minke.zhang@gmail.com>
2025-04-12 17:42:22 -0700

    Add zoom-to-mouse

--- godot/scenes/debug_camera/debug_camera.gd
+++ godot/scenes/debug_camera/debug_camera.gd
@@ -3,6 +3,12 @@ extends Camera2D
 var _pan: bool = false
 var _pan_origin: Vector2
 
+func _zoom(delta: Vector2):
+	var pos := get_global_mouse_position()
+	set_zoom((get_zoom() + delta).clamp(Vector2(1, 1), Vector2(10, 10)))
+	position += pos - get_global_mouse_position()
+
+
 func _input(event: InputEvent):
 	var e = make_input_local(event)
 
@@ -12,16 +18,15 @@ func _input(event: InputEvent):
 	if e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_MIDDLE:
 		if e.is_pressed():
 			_pan = true
-			_pan_origin = e.position
+			_pan_origin = get_global_mouse_position()
 		if e.is_released():
 			_pan = false
 	elif e is InputEventMouseMotion and _pan:
 		var dv = _pan_origin - get_global_mouse_position()
-		offset += dv
+		position += dv
 	# Control zoom behavior.
 	#
-	# TODO(minkezhang): Add support for zooming towards mouse.
 	elif e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_WHEEL_DOWN and e.is_pressed():
-		set_zoom((get_zoom() - Vector2(1, 1)).clamp(Vector2(1, 1), Vector2(10, 10)))
+		_zoom(-Vector2(1, 1))
 	elif e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_WHEEL_UP and e.is_pressed():
-		set_zoom((get_zoom() + Vector2(1, 1)).clamp(Vector2(1, 1), Vector2(10, 10)))	
+		_zoom(Vector2(1, 1))
