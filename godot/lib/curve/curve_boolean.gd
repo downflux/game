fb22f4e43e5d5a6cc373367c42fd969891884b8f
Minke Zhang <minke.zhang@gmail.com>
2025-05-21 03:18:09 -0700

    Update common API

--- godot/lib/curve/curve_boolean.gd
+++ godot/lib/curve/curve_boolean.gd
@@ -4,8 +4,10 @@ class_name DFCurveBoolean
 @export var data: Dictionary[int, bool] = {}
 @export var default_value: bool = false
 
+
 func add_data(d: Dictionary[int, bool]):
 	super.add_data(d)
+
 
 func get_value(timestamp: int) -> bool:
 	return super.get_value(timestamp)
