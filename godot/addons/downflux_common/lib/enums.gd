85db29defbcaef810a4c020f510ce5673d71596f
Minke Zhang <minke.zhang@gmail.com>
2025-05-21 02:41:43 -0700

    Add enums and state keys constant files

---
+++ godot/addons/downflux_common/lib/enums.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name DFEnums
+
+
+enum Permission { PERMISSION_FULL, PERMISSION_NONE }
+enum DataFilter {
+	FILTER_NONE      = 0,
+	FILTER_PLAYERS   = 1,
+	FILTER_UNITS     = 2,
+	FILTER_BUILDINGS = 4,
+}
---
+++ godot/addons/downflux_common/lib/state_keys.gd
@@ -0,0 +1,27 @@
+# Module defines the list of world state dictionary key strings when
+# communicating between server and client.
+#
+# In order to minimize data transfer, we may need compress the key length. This
+# allows us to keep scripts readable.
+#
+# TODO(minkezhang): Move to the common project.
+extends Node
+class_name DFStateKeys
+
+const KDFServerTimestamp = "server_timestamp"
+const KDFState           = "state"
+const KDFPlayers         = "players"
+const KDFUnits           = "units"
+const KDFBuildings       = "buildings"
+
+# Players
+const KDFPlayerUsername = "username"
+const KDFPlayerMoney    = "money"
+const KDFPlayerUnits    = "units"
+const KDFPlayerFaction  = "faction"
+
+# Curves
+const KDFCurveType         = "curve_type"
+const KDFCurveTimestamps   = "timestamps"
+const KDFCurveData         = "data"
+const KDFCurveDefaultValue = "default_value"
