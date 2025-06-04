b5b67135e25a5d4f5064d9dbc59e8de7ba92ec63
Minke Zhang <minke.zhang@gmail.com>
2025-06-04 00:16:15 -0700

    Add DFClientPlayer.UI nodes

--- client/godot/scenes/instances/player/player.gd
+++ client/godot/scenes/instances/player/player.gd
@@ -2,16 +2,37 @@ class_name DFClientPlayer
 extends DFStateBase
 
 @onready var player_state: DFPlayer = $Player
+@onready var username: Label        = $UI/Username
+@onready var faction: Label         = $UI/Faction
+@onready var money: Label           = $UI/Money
+@onready var ui: CanvasLayer        = $UI
 
 ## If set to true, this is the player interacting with this instance of the
 ## client.
-var is_self: bool
+var is_self: bool:
+	set(v):
+		if ui != null:
+			ui.visible = v
+		is_self = v
+
+
+func _ready():
+	ui.visible = is_self
+
+
+func _process(delta):
+	money.text = "$%d" % [player_state.money.get_value(0)]
 
 
 func from_dict(
 	partial: bool,
 	data: Dictionary,
 ):
+	if not partial and DFStateKeys.KDFPlayerUsername in data:
+		username.text = data[DFStateKeys.KDFPlayerUsername]
+	if not partial and DFStateKeys.KDFPlayerFaction in data:
+		faction.text = DFEnums.Faction.keys()[data[DFStateKeys.KDFPlayerFaction]]
+	
 	if DFStateKeys.KDFIsFreed in data:
 		self.is_deleted = true
 		return
