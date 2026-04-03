7e05bfd854fe058b7566cfd0a0d6697d1f473973
Minke Zhang <minke.zhang@gmail.com>
2026-04-03 00:32:23 -0700

    Reference Godot's time package

--- client/example/Example.cs
+++ client/example/Example.cs
@@ -1,17 +1,16 @@
-using Godot;
 using System;
 
-public partial class Example : Node2D
+public partial class Example : Godot.Node2D
 {
 	public Example() {
-		GD.Print("Hello World");
+		Godot.GD.Print("Hello World");
 		Curve<int> x = new();
-		GD.Print(x.Value(100));
+		Godot.GD.Print(x.Value(Godot.Time.GetTicksMsec()));
 	}
 }
 
 public class Curve<T> where T : struct {
-	public Nullable<T> Value(int t) {
+	public Nullable<T> Value(ulong t) {
 		return default(T);
 	}
 }
