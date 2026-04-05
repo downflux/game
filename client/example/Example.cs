d12bda5bb13ef2522e75ab600b0a9096b91acaf6
Minke Zhang <minke.zhang@gmail.com>
2026-04-05 02:29:41 -0700

    Add TweenCurve implmentation to clean up Curve type

--- client/example/Example.cs
+++ client/example/Example.cs
@@ -10,15 +10,66 @@ public partial class Example : Godot.Node2D
 		Curve<int> x = new();
 		Godot.GD.Print(x.Get(Godot.Time.GetTicksMsec(), 30));
 	}
+	
 	public override void _Process(double delta) {
+	}
+}
+
+enum InterpolationType {
+	Linear
+	Step
+	Pulse
+}
+
+public partial class TweenCurve<T> : Godot.Node where T: struct {
+	public delegate ulong GetTimeDelegate();
+	
+	private 
+	private SortedList<ulong, T> schedule = new();
+	private List<(ulong Target, ulong? Source, T Value)> schedule_cache = new();
+	
+	private GetTimeDelegate timer;  // Get the current time.
+	
+	public TweenCurve(GetTimeDelegate f) {
+		this.timer = f;
+	}
+	
+	public TweenCurve() : this(() => Godot.Time.GetTicksMsec()) {
+	}
+	
+	public void Halt() => this.Truncate(this.timer() /* set behavior */ )
+	public void Truncate(ulong t /* stop behavior or hard truncate */) {}
+	public T? Get(ulong t) { return default(T); } // TODO(minkezhang): implmement
+	
+	// Update the Curve schedule.
+	public override void _Process(double delta) {
+		if (!this.schedule_cache.Any()) { return; }  // is_dirty = False
+		
+		foreach(var e in this.schedule_cache) {
+			// If this value is a move op, update the key.
+			if (e.Source.HasValue && this.schedule.ContainsKey(e.Source.Value)) {
+				this.schedule.Remove(e.Source.Value);
+			}
+			
+			// If a value already exists at the target timestamp, overwrite with new
+			// value.
+			if (this.schedule.ContainsKey(e.Target)) {
+				this.schedule.Remove(e.Target);
+			}
+			this.schedule.Add(e.Target, e.Value);
+		}
+		
+		this.schedule_cache.Clear();
 	}
 }
 
 public class Curve<T> where T : struct {
 	private Dictionary<ulong, T> data = new();
-	private Dictionary<ulong, T> write_cache = new();
 	private List<ulong> timestamps = new();
 	private T default_value = default(T);
+	
+	private List<(ulong Timestamp, T Value)> set_ops = new();
+	private List<(ulong Timestamp, double Delay)> shift_ops = new();
 	
 	public Curve() : this(default(T)) {
 	}
@@ -32,7 +83,11 @@ public class Curve<T> where T : struct {
 	}
 	
 	public void Set(ulong t, T v) {
-		this.write_cache[t] = v;
+		this.set_ops.Add((t, v));
+	}
+	
+	public void Shift(ulong t, double delay) {
+		this.shift_ops.Add((t, delay));
 	}
 	
 	public Nullable<T> Get(ulong t, double delta) {
@@ -40,9 +95,9 @@ public class Curve<T> where T : struct {
 	}
 	
 	public void _Process(double delta) {
-		this.write_cache.ToList().ForEach(x => this.data[x.Key] = x.Value);
-		this.write_cache.ToList().ForEach(x => this.timestamps.Add(x.Key));
-		this.write_cache.Clear();
+		this.set_ops.ForEach(x => this.data[x.Timestamp] = x.Value);
+		this.set_ops.ForEach(x => this.timestamps.Add(x.Timestamp));
+		this.set_ops.Clear();
 		this.timestamps.Sort();
 	}
 }
