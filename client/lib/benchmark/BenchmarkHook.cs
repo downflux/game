522894f623447055e12d2a70264a5acfc2f5d6c3
Minke Zhang <minke.zhang@gmail.com>
2026-04-08 16:35:51 -0700

    Add BenchmarkHook TODO

---
+++ client/lib/benchmark/BenchmarkHook.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Downflux.Tests;
+
+// TODO(minkezhang): Implement once GdUnit4Net v6.0.0 is released.
+//
+// See
+// https://godot-gdunit-labs.github.io/gdUnit4/latest/advanced_testing/hooks/
+// for more information.
+/*
+ * public partial class BenchmarkHook : GdUnit4.GdUnitTestSessionHook {
+ *   
+ *   public BenchmarkHook : base("Benchmark", "measures execution time of tests") {
+ *   }
+ *   
+ *   public override GdUnit4.GdUnitResult Startup(GdUnit4.GdUnitTestSession session) {
+ *   }
+ * }
+ */
--- client/lib/curve/CurveTest.cs
+++ client/lib/curve/CurveTest.cs
@@ -6,6 +6,32 @@ namespace Downflux.Tests;
 [TestSuite]
 public class CurveTest {
 		[TestCase]
+		public void BenchmarkGet() {
+			Downflux.Lib.Curve<float> x = new();
+			
+			ulong n_points = 1000;
+			ulong n_runs = 1000000;
+			int server_tps = 10;
+			ulong granularity = 1000;
+			
+			for (ulong i = 0; i < n_points; i++) {
+				x.Schedule((ulong) i * granularity, null, i);
+			}
+			x.Process(1);
+			
+			System.Random seed = new System.Random();
+			
+			System.DateTime start = System.DateTime.Now;
+			for (ulong i = 0; i < n_runs; i++) {
+				x.Get((ulong) seed.Next((int) (n_points * granularity)));
+			}
+			double elapsed = System.DateTime.Now.Subtract(start).TotalSeconds;
+			double n_frames = elapsed * server_tps;
+			// TODO(minkezhang): Write benchmark hook.
+			AssertThat((double) (n_runs) / n_frames).IsGreater(100000);
+		}
+		
+		[TestCase]
 		public void TestGetUlong() {
 			Downflux.Lib.Curve<ulong> x = new();
 			x.Schedule(10, null, 110);
