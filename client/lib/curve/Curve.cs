a480ef085f56573781dadc1023c5ae69786a4a27
Minke Zhang <minke.zhang@gmail.com>
2026-04-08 03:11:09 -0700

    Implement Curve<T>.Get()

--- client/lib/curve/Curve.cs
+++ client/lib/curve/Curve.cs
@@ -12,78 +12,157 @@ enum InterpolationType {
 	Pulse,
 }
 
-public partial class Curve<T> : Node where T: struct {
-	/* Type Declarations */
-	public delegate ulong GetTimeDelegate();
-	public record struct Value(ulong T, T V);
-	
+public partial class CurveNode<T> : Node where T : struct {
+	private Curve<T> curve = new();
+}
+
+public record struct Snapshot<T>(ulong Timestamp, T Value) where T : struct;
+public record struct Interval<T>(Snapshot<T>? Lo, Snapshot<T>? Hi) where T : struct;
+
+public class Curve<T> where T : struct {
 	private SortedList<ulong, T> schedule = new();
 	private List<(ulong Target, ulong? Source, T Value)> schedule_cache = new();
 	
-	private GetTimeDelegate timer;  // Get the current time.
+	private InterpolationType interpolation_type = InterpolationType.Linear;
 	
-	public Curve(GetTimeDelegate f) {
-		this.timer = f;
-	}
+	private Snapshot<T> GetSnapshot(int index) => new (
+		this.schedule.GetKeyAtIndex(index),
+		this.schedule.GetValueAtIndex(index));
 	
-	public Curve() : this(() => Time.GetTicksMsec()) {
-	}
-	
-	// Returns the bounded interval indices of the sorted list of timestamps.
-	private static (int? Lo, int? ID, int? Hi) BinarySearch(IList<ulong> l, ulong v) {
-		(int lo, int hi) = (0, l.Count - 1);
-		while (lo <= hi) {  // binary search
+	/// <summary>
+	/// Get a timestamp which is guaranteed to have an associated value.
+	/// </summary>
+	/// <summary>
+	/// The input timestamp may lie in between tween points. If this is the case,
+	/// return the smallest timestamp which has a definitive associated value. If
+	/// the input timestamp already exists as a defined key in the tween, return
+	/// the timestamp.
+	/// </summary>
+	/// <returns>
+	/// The (K, V) tuple of lower bound. The timestamp of this tuple is guaranteed
+	/// to exist in the curve. Returns null if the input timestamp is less than
+	/// the first defined point.
+	/// </returns>
+	public Snapshot<T>? LowerBound(ulong timestamp) {
+		if (!this.schedule.Any()) {
+			return null;
+		}
+		
+		(int lo, int hi) = (0, this.schedule.Count - 1);
+		while (lo <= hi) {
 			int mid = lo + (hi - lo) / 2;
-			if (v == l[mid]) {
-				return (null, mid, null);
-			}
-			else if (v < l[mid]) {  // mid - 1 < v < mid
-				if (mid - 1 >= 0 && l[mid - 1] < v) {
-					return (mid - 1, null, mid);
+			if (timestamp == this.schedule.GetKeyAtIndex(mid)) {
+				return this.GetSnapshot(mid);
+			} else if (timestamp < this.schedule.GetKeyAtIndex(mid)) {
+				if (mid - 1 >= 0 && this.schedule.GetKeyAtIndex(mid - 1) < timestamp) { // mid - 1 < v < mid
+					return this.GetSnapshot(mid - 1);
+				} else {
+					hi = mid - 1;
 				}
-				hi = mid - 1;
-			}
-			else {  // mid < v < mid + 1
-				if (mid + 1 <= l.Count - 1 && v < l[mid - 1]) {
-					return (mid, null, mid + 1);
-				}
+			} else {
 				lo = mid + 1;
 			}
 		}
-		return (null, null, null);
+		
+		if (hi <= 0) {  // mid = -1
+			return null;
+		}
+		
+		return this.GetSnapshot(hi);  // mid = this.snapshot.Count - 1
 	}
 	
-	public (Value? Lo, Value? V, Value? Hi) GetInterval(ulong timestamp) {
-		var bounds = Curve<T>.BinarySearch(this.schedule.Keys, timestamp);
-		return (
-			bounds.Lo.HasValue ? new Curve<T>.Value(
-				this.schedule.GetKeyAtIndex(bounds.Lo.Value),
-				this.schedule.GetValueAtIndex(bounds.Lo.Value)) : null,
-			bounds.ID.HasValue ? new Curve<T>.Value(
-				this.schedule.GetKeyAtIndex(bounds.ID.Value),
-				this.schedule.GetValueAtIndex(bounds.ID.Value)) : null,
-			bounds.Hi.HasValue ? new Curve<T>.Value(
-				this.schedule.GetKeyAtIndex(bounds.Hi.Value),
-				this.schedule.GetValueAtIndex(bounds.Hi.Value)) : null);
+	public Snapshot<T>? UpperBound(ulong timestamp) {
+		if (!this.schedule.Any()) {
+			return null;
+		}
+		
+		var lower = this.LowerBound(timestamp);
+		if (!lower.HasValue) {
+			return this.GetSnapshot(0);
+		} else if(lower.Value.Timestamp == timestamp) {
+			return lower;
+		} else {
+			var index = this.schedule.IndexOfKey(lower.Value.Timestamp);
+			if (index < this.schedule.Count - 1) {
+				return this.GetSnapshot(index + 1);
+			}
+		}
+		
+		return null;
 	}
 	
+	/*
 	public void Halt() => this.Truncate(this.timer(), false);
 	public void Truncate(ulong t, bool keep) {
-	/*
 		int i = this.schedule.
 		if keep {
 			
 		}
-		*/
 	}
+	 */
 	
 	public void Schedule(ulong t, ulong? s, T v) => this.schedule_cache.Add((t, s, v));
 	
-	public T? Get(ulong t) { return default(T); } // TODO(minkezhang): implmement
+	public Snapshot<T>? Get(ulong t) {
+		var (lo, hi) = (this.LowerBound(t), this.UpperBound(t));
+		if (lo.HasValue) {
+			if (!hi.HasValue) {
+				return new Snapshot<T>(t, lo.Value.Value);
+			} else {
+				float scale_factor = ((float) (t - lo.Value.Timestamp)) / ((float) (hi.Value.Timestamp - lo.Value.Timestamp));
+				switch (this.interpolation_type) {
+					case InterpolationType.Linear:
+						switch (typeof(T)) {
+							case Type type when (type == typeof(ulong)):
+								(ulong? v_hi_ulong, ulong? v_lo_ulong) = (hi.Value.Value as ulong?, lo.Value.Value as ulong?);
+								if (!v_hi_ulong.HasValue || !v_lo_ulong.HasValue) {
+									return null;
+								}
+								T? val_ulong = (ulong) System.Math.Round(
+									(float) v_lo_ulong.Value + (((float) v_hi_ulong.Value - (float) v_lo_ulong.Value) * scale_factor)) as T?;
+								if (!val_ulong.HasValue) {
+									return null;
+								}
+								return new Snapshot<T>(t, val_ulong.Value);
+							case Type type when (type == typeof(int)):
+								(int? v_hi_int, int? v_lo_int) = (hi.Value.Value as int?, lo.Value.Value as int?);
+								if (!v_hi_int.HasValue || !v_lo_int.HasValue) {
+									return null;
+								}
+								T? val_int = (int) System.Math.Round(
+									(float) v_lo_int.Value + (((float) v_hi_int.Value - (float) v_lo_int.Value) * scale_factor)) as T?;
+								if (!val_int.HasValue) {
+									return null;
+								}
+								return new Snapshot<T>(t, val_int.Value);
+							case Type type when (type == typeof(float)):
+								(float? v_hi_float, float? v_lo_float) = (hi.Value.Value as float?, lo.Value.Value as float?);
+								if (!v_hi_float.HasValue || !v_lo_float.HasValue) {
+									return null;
+								}
+								T? val_float = (v_lo_float.Value + ((v_hi_float.Value - v_lo_float.Value) * scale_factor)) as T?;
+								if (!val_float.HasValue) {
+									return null;
+								}
+								return new Snapshot<T>(t, val_float.Value);
+							default:
+								return null;
+						}
+					case InterpolationType.Step:
+						return new Snapshot<T>(t, lo.Value.Value);
+					default:
+						break;
+				}
+			}
+		}
+		return null;
+	}
 	
 	// Update the Curve schedule.
-	public override void _Process(double delta) {
-		if (!this.schedule_cache.Any()) { return; }  // is_dirty = False
+	public void Process(double delta) {
+		if (!this.schedule_cache.Any()) {  // is_dirty = False
+			return;
+		}
 		
 		foreach(var e in this.schedule_cache) {
 			// If this value is a move op, update the key.
@@ -94,9 +173,10 @@ public partial class Curve<T> : Node where T: struct {
 			// If a value already exists at the target timestamp, overwrite with new
 			// value.
 			if (this.schedule.ContainsKey(e.Target)) {
-				this.schedule.Remove(e.Target);
+				this.schedule.SetValueAtIndex(this.schedule.IndexOfKey(e.Target), e.Value);
+			} else {
+				this.schedule.Add(e.Target, e.Value);
 			}
-			this.schedule.Add(e.Target, e.Value);
 		}
 		
 		this.schedule_cache.Clear();
--- client/lib/curve/CurveTest.cs
+++ client/lib/curve/CurveTest.cs
@@ -6,13 +6,74 @@ namespace GdUnit4.Tests;
 [TestSuite]
 public class CurveTest {
 		[TestCase]
-		public void IsEqual() {
-			AssertThat(1).Equals(1);
+		public void TestGetUlong() {
+			Downflux.Lib.Curve<ulong> x = new();
+			x.Schedule(10, null, 110);
+			x.Schedule(20, null, 120);
+			x.Schedule(30, null, 130);
+			x.Process(1);
+			
+			AssertThat(x.Get(0)).IsNull();
+			AssertThat(x.Get(15)).IsEqual(((Downflux.Lib.Snapshot<ulong>) new (15, 115)));
+			AssertThat(x.Get(35)).IsEqual(((Downflux.Lib.Snapshot<ulong>) new (35, 130)));
 		}
 		
-		[TestCase] 
-		[RequireGodotRuntime] // ← Add this for Godot-dependent tests
-		public void IsNotNull() {
-				 AssertThat(new Godot.Node2D()).IsNotNull();
+		[TestCase]
+		public void TestGetInt() {
+			Downflux.Lib.Curve<int> x = new();
+			x.Schedule(10, null, 110);
+			x.Schedule(20, null, 120);
+			x.Schedule(30, null, 130);
+			x.Process(1);
+			
+			AssertThat(x.Get(0)).IsNull();
+			AssertThat(x.Get(15)).IsEqual(((Downflux.Lib.Snapshot<int>) new (15, 115)));
+			AssertThat(x.Get(35)).IsEqual(((Downflux.Lib.Snapshot<int>) new (35, 130)));
+		}
+		
+		[TestCase]
+		public void TestGetFloat() {
+			Downflux.Lib.Curve<float> x = new();
+			x.Schedule(10, null, 110);
+			x.Schedule(20, null, 120);
+			x.Schedule(30, null, 130);
+			x.Process(1);
+			
+			AssertThat(x.Get(0)).IsNull();
+			AssertThat(x.Get(15)).IsEqual(((Downflux.Lib.Snapshot<float>) new (15, 115)));
+			AssertThat(x.Get(35)).IsEqual(((Downflux.Lib.Snapshot<float>) new (35, 130)));
+		}
+		
+		[TestCase]
+		public void TestLowerBound() {
+			Downflux.Lib.Curve<int> x = new();
+			x.Schedule(10, null, 110);
+			x.Schedule(20, null, 120);
+			x.Schedule(30, null, 130);
+			x.Process(1);
+			
+			AssertThat(x.LowerBound(0)).IsNull();
+			AssertThat(x.LowerBound(10)).IsEqual(((Downflux.Lib.Snapshot<int>) new (10, 110)));
+			AssertThat(x.LowerBound(15)).IsEqual(((Downflux.Lib.Snapshot<int>) new (10, 110)));
+			AssertThat(x.LowerBound(35)).IsEqual(((Downflux.Lib.Snapshot<int>) new (30, 130)));
+		}
+		
+		[TestCase]
+		public void TestUpperBound() {
+			Downflux.Lib.Curve<int> x = new();
+			x.Schedule(10, null, 110);
+			x.Schedule(20, null, 120);
+			x.Schedule(30, null, 130);
+			x.Process(1);
+			
+			AssertThat(x.UpperBound(0)).IsEqual(((Downflux.Lib.Snapshot<int>) new (10, 110)));
+			AssertThat(x.UpperBound(10)).IsEqual(((Downflux.Lib.Snapshot<int>) new (10, 110)));
+			AssertThat(x.UpperBound(15)).IsEqual(((Downflux.Lib.Snapshot<int>) new (20, 120)));
+			AssertThat(x.UpperBound(35)).IsNull();
+		}
+		
+		[TestCase]
+		[RequireGodotRuntime]
+		public void TestNull() {
 		}
 }
